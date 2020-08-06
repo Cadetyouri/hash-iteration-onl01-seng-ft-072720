3f4ec27923c94b4e369bdad481b0827263702078
Youri Cadet <cadetjesusyouri@gmail.com>
2020-08-06 05:26:12 +0000

    Automatically backed up by Learn

--- lib/birthday.rb
+++ lib/birthday.rb
@@ -11,6 +11,7 @@ def happy_birthday(birthday_kids)
   end
 end
 
+
 def age_appropriate_birthday(birthday_kids)
   birthday_kids.each do |kids_name, age|
     if age <= 12
@@ -20,4 +21,3 @@ def age_appropriate_birthday(birthday_kids)
     end
   end
 end
-
